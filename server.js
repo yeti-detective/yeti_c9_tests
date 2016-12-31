cb8e189397a6d716e78287d5c2eca6a8743ae962
Christopher M Brown <yeti.detective@gmail.com>
2016-12-31 16:04:17 +0000

    Final commit of 2016!

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var hTml =  fs.readFileSync('index.html').toString();
+var dateObj = {};
+
+function convertDate(daTe){
+    var date = new Date(daTe);
+    var naturalDate = months[date.getMonth()] + ' ' + date.getDate().toString() + ', ' + date.getFullYear().toString();
+    var unixDate = Date.parse(date);
+    dateObj["unix"] = unixDate;
+    dateObj["natural"] = naturalDate;
+}
 
 app.get('/:date', function(req, res){
-    // res.setRequestHeader(200, 'Content-Type: text/html');
-    if(Boolean(Date.parse(req.params.date))){
-        var date = new Date(req.params.date);
-        var naturalDate = months[date.getMonth()] + ' ' + date.getDate().toString() + ', ' + date.getFullYear().toString();
+    if(Boolean(parseInt(req.params.date))){
+        if(Boolean(Date(parseInt(req.params.date)))){
+            convertDate(parseInt(req.params.date));
+        }
     }
-    res.send(
-        `
-        <h1>Yeti's Timestamp Generator</h1>
-        <p>Add a natural-language date</p>
-        <p class="example">ie: "February 14, 1998"</p>
-        <p>or a unix timecode</p>
-        <p class="example"></p>
-        `
-    );
+    if(Boolean(Date.parse(req.params.date))){
+        convertDate(req.params.date);
+    }
+        
+    res.send(hTml + '\n' + '<h1 class="date">' + JSON.stringify(dateObj)) + '</h1>';
 });
 
 app.get('*', function(req, res){
-    res.send("put a date after the URL");
+    res.send(hTml);
 });
 
 app.listen(8080, function(){
